2f0c3534bc585a91190b7876639b0bf29b16a2ca
Crowdbotics <team@crowdbotics.com>
2020-03-25 08:06:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/healthlynked_15051/MainActivity.java
@@ -0,0 +1,15 @@
+package com.healthlynked_15051;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "healthlynked_15051";
+    }
+}
